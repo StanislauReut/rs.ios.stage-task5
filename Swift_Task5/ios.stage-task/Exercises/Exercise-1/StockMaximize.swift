266ec573a86dc6ac9312a4259e61507645666351
StanislauReut <52281305+StanislauReut@users.noreply.github.com>
2021-06-14 18:18:48 +0300

    it's done

--- Swift_Task5/ios.stage-task/Exercises/Exercise-1/StockMaximize.swift
+++ Swift_Task5/ios.stage-task/Exercises/Exercise-1/StockMaximize.swift
@@ -1,8 +1,25 @@
 import Foundation
 
 class StockMaximize {
-
+    
     func countProfit(prices: [Int]) -> Int {
-        return -1
+        if prices.count == 0 || prices.count == 1 {
+            return 0
+        }
+        
+        var profit = 0
+        var dayWhenCanSell = prices
+        
+        for i in 0..<prices.count - 1 {
+            dayWhenCanSell.remove(at: 0)
+            guard let maxPrice = dayWhenCanSell.sorted().last else {
+                return 0
+            }
+            if prices[i] < maxPrice{
+                profit += maxPrice - prices[i]
+            }
+        }
+        return profit
     }
+    
 }
